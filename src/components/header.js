815720d6f04437dbb300aef743ae98ba1b5cbfd2
willieLjohnson <liwa.johnson@gmail.com>
2019-05-22 22:57:37 -0400

    Update main page adding flex and sidebar

--- src/components/header.js
+++ src/components/header.js
@@ -5,24 +5,24 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#f5f5f5`,
-      marginBottom: `3rem`,
+      background: "#f5f5f5",
+      marginBottom: "3rem",
       borderBottom: "2px solid #e6e6e6",
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin: "0 auto",
         maxWidth: 980,
-        padding: `1.45rem 1.0875rem`,
+        padding: "1.45rem 1.0875rem",
       }}
     >
       <h1 style={{ margin: 0, textAlign: "center", fontSize: "18px" }}>
         <Link
           to="/"
           style={{
-            color: `black`,
-            textDecoration: `none`,
+            color: "black",
+            textDecoration: "none",
           }}
         >
           {siteTitle}
@@ -37,7 +37,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: "",
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Media from "react-media"
 
 import Header from "./header"
+import Sidebar from "./sidebar"
 import "./layout.css"
 import "../styles/layout-override.css"
 
@@ -29,18 +31,59 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            margin: "0 auto",
+            maxWidth: 980,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            height: "100%",
           }}
         >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <Media query={{ maxWidth: 848 }}>
+            {matches =>
+              matches ? (
+                <div
+                  style={{
+                    margin: "0 auto",
+                    maxWidth: 980,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    height: "100%",
+                    padding: "25px",
+                  }}
+                >
+                  <main style={{ flex: 1 }}>{children}</main>
+                </div>
+              ) : (
+                <div
+                  style={{
+                    margin: "0 auto",
+                    maxWidth: 980,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    height: "100%",
+                    padding: "25px",
+                  }}
+                >
+                  <main style={{ flex: 2.5, paddingRight: "30px" }}>
+                    {children}
+                  </main>
+                  <div style={{ flex: 1 }}>
+                    <Sidebar
+                      title="Content"
+                      description="Posts on life, technology, and games."
+                    />
+                    <Sidebar
+                      title="About author"
+                      description="I am a Software Developer specializing in iOS and Unity development."
+                    />
+                  </div>
+                </div>
+              )
+            }
+          </Media>
         </div>
       </>
     )}
