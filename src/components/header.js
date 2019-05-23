278b65dfb9d9c21295308631dc0513db0c32d291
willieLjohnson <liwa.johnson@gmail.com>
2019-05-23 14:38:59 -0400

    updatestyles

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: "#f5f5f5",
+      background: "#fdfffc",
       marginBottom: "3rem",
       borderBottom: "2px solid #e6e6e6",
     }}
@@ -30,14 +30,14 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: "",
-}
+};
 
-export default Header
+export default Header;
