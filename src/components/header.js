18266927717b912d77b0fa1d067f543aadf90d43
willieLjohnson <liwa.johnson@gmail.com>
2019-05-22 22:21:38 -0400

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,23 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `#f5f5f5`,
+      marginBottom: `3rem`,
+      borderBottom: "2px solid #e6e6e6",
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 980,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, textAlign: "center", fontSize: "18px" }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
